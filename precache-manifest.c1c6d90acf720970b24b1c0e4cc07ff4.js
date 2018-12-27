8dbce0560a16b71be3b2b07c28e0d36001b13721
chenghan <chenghan@mail.apex.com.tw>
2018-12-27 10:58:16 +0800

    Updates

---
+++ precache-manifest.c1c6d90acf720970b24b1c0e4cc07ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/t-quote/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df9fded1ae0424658a78",
+    "url": "/t-quote/static/js/runtime~main.df9fded1.js"
+  },
+  {
+    "revision": "83689f15dd38436679a2",
+    "url": "/t-quote/static/js/main.83689f15.chunk.js"
+  },
+  {
+    "revision": "66aab8b6616d9861f811",
+    "url": "/t-quote/static/js/1.66aab8b6.chunk.js"
+  },
+  {
+    "revision": "83689f15dd38436679a2",
+    "url": "/t-quote/static/css/main.d0c56c37.chunk.css"
+  },
+  {
+    "revision": "75cf2b78865f092b446306442c25e313",
+    "url": "/t-quote/index.html"
+  }
+];
